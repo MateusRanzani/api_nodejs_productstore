7fe68074c320c727a5246ad861f4fbb314eb492a
MateusRanzani <mateusranzani@hotmail.com>
2022-09-06 12:56:50 -0300

    Update comments route

--- routes/commentsRoute.tsx
+++ routes/commentsRoute.tsx
@@ -24,4 +24,53 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.post("/:id", async (req, res) => {
+  const idParam = req.params.id;
+  const { user_name, comment } = req.body;
+
+  const commentArrayFind = await Comments.findIndex(
+    (comment) => comment.id_product === idParam
+  );
+
+  if (!user_name || !comment) {
+    res.status(422).json({ error: "Preencha todos os campos obrigatórios" });
+    return;
+  }
+
+  const auxComment = {
+    user_name: user_name,
+    date: new Date(),
+    comment: comment,
+  };
+
+  try {
+    await Comments[commentArrayFind].comments.push(auxComment);
+    res
+      .status(201)
+      .json({ message: "Comentario inserido com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const idSearch = req.params.id;
+
+  const commentDelete = await Comments.findIndex(
+    (comment) => comment.id_product === idSearch
+  );
+
+  if (commentDelete < 0) {
+    res.status(422).json({ message: "Erro ao deletar o comentário" });
+    return;
+  }
+
+  try {
+    await Comments.splice(commentDelete, 1);
+    res.status(200).json({ message: "Comentário excluído com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
 module.exports = router;
